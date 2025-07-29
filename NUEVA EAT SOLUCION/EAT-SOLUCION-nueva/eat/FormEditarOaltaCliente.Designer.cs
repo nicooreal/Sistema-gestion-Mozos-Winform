3e1ca723fce3700c9282143f508e042dbde16bcb
nicooreal <nicoreal99@gmail.com>
2025-07-29 16:33:21 -0300

    Winform alta o editar cliente

---
+++ NUEVA EAT SOLUCION/EAT-SOLUCION-nueva/eat/FormEditarOaltaCliente.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace eat
+{
+    partial class FormEditarOaltaCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.textBoxDNI = new System.Windows.Forms.TextBox();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.textBoxCuil = new System.Windows.Forms.TextBox();
+            this.textBoxCorreo = new System.Windows.Forms.TextBox();
+            this.textBoxTelefono = new System.Windows.Forms.TextBox();
+            this.textBoxObservacion = new System.Windows.Forms.TextBox();
+            this.textBoxApellido = new System.Windows.Forms.TextBox();
+            this.labelID = new System.Windows.Forms.Label();
+            this.labelDni = new System.Windows.Forms.Label();
+            this.labelNombre = new System.Windows.Forms.Label();
+            this.LabelCuil = new System.Windows.Forms.Label();
+            this.labelFechaAlta = new System.Windows.Forms.Label();
+            this.labelCorreo = new System.Windows.Forms.Label();
+            this.labelTelefono = new System.Windows.Forms.Label();
+            this.labelObservacion = new System.Windows.Forms.Label();
+            this.labelApellido = new System.Windows.Forms.Label();
+            this.buttonCancelar = new System.Windows.Forms.Button();
+            this.buttonAceptar = new System.Windows.Forms.Button();
+            this.dateTimePickerFechaAlta = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(130, 88);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(100, 20);
+            this.textBoxID.TabIndex = 0;
+            // 
+            // textBoxDNI
+            // 
+            this.textBoxDNI.Location = new System.Drawing.Point(130, 265);
+            this.textBoxDNI.Name = "textBoxDNI";
+            this.textBoxDNI.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDNI.TabIndex = 1;
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(130, 172);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNombre.TabIndex = 2;
+            // 
+            // textBoxCuil
+            // 
+            this.textBoxCuil.Location = new System.Drawing.Point(130, 219);
+            this.textBoxCuil.Name = "textBoxCuil";
+            this.textBoxCuil.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCuil.TabIndex = 3;
+            // 
+            // textBoxCorreo
+            // 
+            this.textBoxCorreo.Location = new System.Drawing.Point(517, 88);
+            this.textBoxCorreo.Name = "textBoxCorreo";
+            this.textBoxCorreo.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCorreo.TabIndex = 5;
+            // 
+            // textBoxTelefono
+            // 
+            this.textBoxTelefono.Location = new System.Drawing.Point(517, 128);
+            this.textBoxTelefono.Name = "textBoxTelefono";
+            this.textBoxTelefono.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTelefono.TabIndex = 6;
+            // 
+            // textBoxObservacion
+            // 
+            this.textBoxObservacion.Location = new System.Drawing.Point(517, 172);
+            this.textBoxObservacion.Name = "textBoxObservacion";
+            this.textBoxObservacion.Size = new System.Drawing.Size(100, 20);
+            this.textBoxObservacion.TabIndex = 7;
+            // 
+            // textBoxApellido
+            // 
+            this.textBoxApellido.Location = new System.Drawing.Point(130, 128);
+            this.textBoxApellido.Name = "textBoxApellido";
+            this.textBoxApellido.Size = new System.Drawing.Size(100, 20);
+            this.textBoxApellido.TabIndex = 8;
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Location = new System.Drawing.Point(82, 91);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(18, 13);
+            this.labelID.TabIndex = 9;
+            this.labelID.Text = "ID";
+            this.labelID.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelDni
+            // 
+            this.labelDni.AutoSize = true;
+            this.labelDni.Location = new System.Drawing.Point(76, 268);
+            this.labelDni.Name = "labelDni";
+            this.labelDni.Size = new System.Drawing.Size(26, 13);
+            this.labelDni.TabIndex = 10;
+            this.labelDni.Text = "DNI";
+            // 
+            // labelNombre
+            // 
+            this.labelNombre.AutoSize = true;
+            this.labelNombre.Location = new System.Drawing.Point(67, 172);
+            this.labelNombre.Name = "labelNombre";
+            this.labelNombre.Size = new System.Drawing.Size(44, 13);
+            this.labelNombre.TabIndex = 11;
+            this.labelNombre.Text = "Nombre";
+            // 
+            // LabelCuil
+            // 
+            this.LabelCuil.AutoSize = true;
+            this.LabelCuil.Location = new System.Drawing.Point(76, 215);
+            this.LabelCuil.Name = "LabelCuil";
+            this.LabelCuil.Size = new System.Drawing.Size(24, 13);
+            this.LabelCuil.TabIndex = 12;
+            this.LabelCuil.Text = "Cuil";
+            // 
+            // labelFechaAlta
+            // 
+            this.labelFechaAlta.AutoSize = true;
+            this.labelFechaAlta.Location = new System.Drawing.Point(441, 222);
+            this.labelFechaAlta.Name = "labelFechaAlta";
+            this.labelFechaAlta.Size = new System.Drawing.Size(57, 13);
+            this.labelFechaAlta.TabIndex = 13;
+            this.labelFechaAlta.Text = "Fecha alta";
+            // 
+            // labelCorreo
+            // 
+            this.labelCorreo.AutoSize = true;
+            this.labelCorreo.Location = new System.Drawing.Point(464, 91);
+            this.labelCorreo.Name = "labelCorreo";
+            this.labelCorreo.Size = new System.Drawing.Size(38, 13);
+            this.labelCorreo.TabIndex = 14;
+            this.labelCorreo.Text = "Correo";
+            // 
+            // labelTelefono
+            // 
+            this.labelTelefono.AutoSize = true;
+            this.labelTelefono.Location = new System.Drawing.Point(453, 128);
+            this.labelTelefono.Name = "labelTelefono";
+            this.labelTelefono.Size = new System.Drawing.Size(49, 13);
+            this.labelTelefono.TabIndex = 15;
+            this.labelTelefono.Text = "Telefono";
+            // 
+            // labelObservacion
+            // 
+            this.labelObservacion.AutoSize = true;
+            this.labelObservacion.Location = new System.Drawing.Point(435, 172);
+            this.labelObservacion.Name = "labelObservacion";
+            this.labelObservacion.Size = new System.Drawing.Size(67, 13);
+            this.labelObservacion.TabIndex = 16;
+            this.labelObservacion.Text = "Observacion";
+            // 
+            // labelApellido
+            // 
+            this.labelApellido.AutoSize = true;
+            this.labelApellido.Location = new System.Drawing.Point(67, 131);
+            this.labelApellido.Name = "labelApellido";
+            this.labelApellido.Size = new System.Drawing.Size(44, 13);
+            this.labelApellido.TabIndex = 17;
+            this.labelApellido.Text = "Apellido";
+            // 
+            // buttonCancelar
+            // 
+            this.buttonCancelar.Location = new System.Drawing.Point(194, 364);
+            this.buttonCancelar.Name = "buttonCancelar";
+            this.buttonCancelar.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancelar.TabIndex = 18;
+            this.buttonCancelar.Text = "Cancelar";
+            this.buttonCancelar.UseVisualStyleBackColor = true;
+            // 
+            // buttonAceptar
+            // 
+            this.buttonAceptar.Location = new System.Drawing.Point(498, 364);
+            this.buttonAceptar.Name = "buttonAceptar";
+            this.buttonAceptar.Size = new System.Drawing.Size(75, 23);
+            this.buttonAceptar.TabIndex = 19;
+            this.buttonAceptar.Text = "Aceptar";
+            this.buttonAceptar.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePickerFechaAlta
+            // 
+            this.dateTimePickerFechaAlta.Location = new System.Drawing.Point(517, 219);
+            this.dateTimePickerFechaAlta.Name = "dateTimePickerFechaAlta";
+            this.dateTimePickerFechaAlta.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerFechaAlta.TabIndex = 20;
+            // 
+            // FormEditarOaltaCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePickerFechaAlta);
+            this.Controls.Add(this.buttonAceptar);
+            this.Controls.Add(this.buttonCancelar);
+            this.Controls.Add(this.labelApellido);
+            this.Controls.Add(this.labelObservacion);
+            this.Controls.Add(this.labelTelefono);
+            this.Controls.Add(this.labelCorreo);
+            this.Controls.Add(this.labelFechaAlta);
+            this.Controls.Add(this.LabelCuil);
+            this.Controls.Add(this.labelNombre);
+            this.Controls.Add(this.labelDni);
+            this.Controls.Add(this.labelID);
+            this.Controls.Add(this.textBoxApellido);
+            this.Controls.Add(this.textBoxObservacion);
+            this.Controls.Add(this.textBoxTelefono);
+            this.Controls.Add(this.textBoxCorreo);
+            this.Controls.Add(this.textBoxCuil);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.textBoxDNI);
+            this.Controls.Add(this.textBoxID);
+            this.Name = "FormEditarOaltaCliente";
+            this.Text = "FormEditarOaltaCliente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.TextBox textBoxDNI;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.TextBox textBoxCuil;
+        private System.Windows.Forms.TextBox textBoxCorreo;
+        private System.Windows.Forms.TextBox textBoxTelefono;
+        private System.Windows.Forms.TextBox textBoxObservacion;
+        private System.Windows.Forms.TextBox textBoxApellido;
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.Label labelDni;
+        private System.Windows.Forms.Label labelNombre;
+        private System.Windows.Forms.Label LabelCuil;
+        private System.Windows.Forms.Label labelFechaAlta;
+        private System.Windows.Forms.Label labelCorreo;
+        private System.Windows.Forms.Label labelTelefono;
+        private System.Windows.Forms.Label labelObservacion;
+        private System.Windows.Forms.Label labelApellido;
+        private System.Windows.Forms.Button buttonCancelar;
+        private System.Windows.Forms.Button buttonAceptar;
+        private System.Windows.Forms.DateTimePicker dateTimePickerFechaAlta;
+    }
+}
